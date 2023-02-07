093578ea8fe196f441f40ceafb9c37756f2d9015
augustov58 <augustovalbuen@gmail.com>
2023-02-07 00:37:55 -0500

    Modified Bot

--- CryptoBot.py
+++ CryptoBot.py
@@ -1,21 +1,28 @@
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from tracker import get_prices
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-# Helooooo
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hola Sexy")
+telegram_bot_token = "5860776780:AAESmrwFBqpJXaKHXIr0PQ2a9jKs9DRVJkY"
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('5860776780:AAESmrwFBqpJXaKHXIr0PQ2a9jKs9DRVJkY').build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+updater = Updater(token=telegram_bot_token, use_context=True)
+dispatcher = updater.dispatcher
 
 
+def start(update, context):
+    chat_id = update.effective_chat.id
+    message = ""
+
+    crypto_data = get_prices()
+    for i in crypto_data:
+        coin = crypto_data[i]["coin"]
+        price = crypto_data[i]["price"]
+        change_day = crypto_data[i]["change_day"]
+        change_hour = crypto_data[i]["change_hour"]
+        message += f"Coin: {coin}\nPrice: ${price:,.2f}\nHour Change: {change_hour:.3f}%\nDay Change: {change_day:.3f}%\n\n"
+
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+
+dispatcher.add_handler(CommandHandler("start", start))
+updater.start_polling()
